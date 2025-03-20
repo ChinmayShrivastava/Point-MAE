16f101a5324dafac833908323a5aa0a7b75271e7
Chinmay Shrivastava <79005030+ChinmayShrivastava@users.noreply.github.com>
2025-03-19 21:47:55 -0700

    Update misc.py

--- utils/misc.py
+++ utils/misc.py
@@ -10,14 +10,17 @@ from collections import abc
 from pointnet2_ops import pointnet2_utils
 
 
-def fps(data, number):
+def fps(data, number, return_idx=False):
     '''
         data B N 3
         number int
     '''
     fps_idx = pointnet2_utils.furthest_point_sample(data, number) 
     fps_data = pointnet2_utils.gather_operation(data.transpose(1, 2).contiguous(), fps_idx).transpose(1,2).contiguous()
-    return fps_data
+    if return_idx:
+        return fps_data, fps_idx
+    else:
+        return fps_data
 
 
 def worker_init_fn(worker_id):
